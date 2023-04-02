420ff48d4b1f223077e619512ddd445fb9d75cb2
tonga2402 <gbalmaceda@live.com>
2023-04-02 03:48:33 -0300

    media queries

--- code.js
+++ code.js
@@ -1,6 +1,26 @@
 const dayDark = document.querySelector("#site");
 const button = document.querySelector("#cambiarTema");
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropDownNav = document.querySelector(".dropdown-nav");
+const buttonLi = document.querySelector("#cambiarTemaLi");
+
+
 
 button.addEventListener("click", function(){
+    dayDark.classList.toggle("day");
+})
+
+toggleBtn.onclick = function(){
+    dropDownNav.classList.toggle("open")
+    const isOpen = dropDownNav.classList.contains("open")
+
+    toggleBtnIcon.classList = isOpen
+    ? "fa-solid fa-xmark"
+    : "fa-solid fa-bars";
+    
+};
+
+buttonLi.addEventListener("click", function(){
     dayDark.classList.toggle("day");
 })
